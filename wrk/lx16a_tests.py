74a422352c82df0e0edf106969cd39ba5fadaa2e
Sergey Kryukov <kryukovS310@gmail.com>
2023-10-10 22:07:46 +0100

    all hail HellHound

--- wrk/lx16a_tests.py
+++ wrk/lx16a_tests.py
@@ -70,13 +70,49 @@ if __name__ == '__main__':
     print(m3.read_angle(17))
     """
 
-    for i in [3, 4, 5, 21, 22, 23]:
+    for i in [9, 10, 11]:
         m1.read_values(i)
-    for i in [6, 12]:
+    for i in [15, 16, 17]:
         m2.read_values(i)
-    for i in [9, 10, 11, 15, 16, 17]:
+    for i in [21, 22, 23]:
         m3.read_values(i)
-    for i in [18, 24]:
+    for i in [3, 4, 5]:
         m4.read_values(i)
-    
 
+    
+    #time.sleep(3)
+
+    angle0 = 0
+    m1.move_servo_to_angle(11, angle0, 1000)
+    m2.move_servo_to_angle(17, angle0, 1000)
+    m3.move_servo_to_angle(23, angle0, 1000)
+    m4.move_servo_to_angle(5, angle0, 1000)
+
+    spd = 1000
+
+    angle1 = 15 # 30 -> 60
+    m1.move_servo_to_angle(10, angle1, spd)
+    m2.move_servo_to_angle(16, angle1, spd)
+    m3.move_servo_to_angle(22, angle1, spd)
+    m4.move_servo_to_angle(4, angle1, spd)
+
+    angle2 = 45 # 45 -> 90
+    m1.move_servo_to_angle(9, angle2, spd)
+    m2.move_servo_to_angle(15, angle2, spd)
+    m3.move_servo_to_angle(21, angle2, spd)
+    m4.move_servo_to_angle(3, angle2, spd)
+
+    time.sleep(1)
+    spd2 = 1000
+
+    angle1 = 130 # 30 -> 60
+    m1.move_servo_to_angle(10, angle1, spd2)
+    m2.move_servo_to_angle(16, angle1, spd2)
+    m3.move_servo_to_angle(22, angle1, spd2)
+    m4.move_servo_to_angle(4, angle1, spd2)
+
+    angle2 = 100 # 45 -> 90
+    m1.move_servo_to_angle(9, angle2, spd2)
+    m2.move_servo_to_angle(15, angle2, spd2)
+    m3.move_servo_to_angle(21, angle2, spd2)
+    m4.move_servo_to_angle(3, angle2, spd2)
